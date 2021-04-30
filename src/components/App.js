a1225284845bb56e31c23111cab8fbc4cd0e88a7
Zach Frohardt <zfrohardt@gmail.com>
2021-04-30 16:10:59 -0700

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,70 @@ import React, { Component } from "react";
 import "../App.css";
 import Nav from "./Nav";
 import hogs from "../porkers_data";
-import HelloWorld from "./HelloWorld";
+import Filters from './Filters';
+import PigList from './PigList';
+
+const filterFunctions = {
+  'all': hog => true,
+  'greased': hog => hog.greased,
+  'ungreased': hog => !hog.greased,
+}
+
+const sortFunctions = {
+  'none': (x, y) => 0,
+  'name': (x, y) => x.name < y.name,
+  'weight': (x, y) => x.weight - y.weight,
+}
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filter: 'all',
+      sort: 'none',
+      hogs: [],
+    }
+  }
+
+  componentDidMount() {
+    let hogsWithImages = hogs.map(this.getHogImages);
+    this.setState({
+      hogs: hogsWithImages,
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <Nav />
-        <HelloWorld />
+        <Filters updateFilter={this.updateFilter} updateSort={this.updateSort} />
+        <PigList hogs={this.applySortAndFilter()} />
       </div>
     );
+  }
+
+  applySortAndFilter = () => {
+    return (
+      this.state.hogs.filter(filterFunctions[this.state.filter])
+      .sort(sortFunctions[this.state.sort])
+    );
+  }
+
+  updateFilter = filter => {
+    this.setState({
+      filter: filter,
+    })
+  }
+
+  updateSort = sort => {
+    this.setState({
+      sort: sort,
+    })
+  }
+
+  getHogImages = hog => {
+    hog.img = require(`../hog-imgs/${hog.name.toLowerCase().split(" ").join("_")}.jpg`);
+    return hog;
   }
 }
 
